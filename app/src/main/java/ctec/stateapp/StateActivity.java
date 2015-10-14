af284041ec04417dfef84c032e82bf942a5a4d4c
Bodie Shane <bodieshane68@gmail.com>
2015-10-14 09:54:37 -0600

    I finished the app to add editing

--- app/src/main/java/ctec/stateapp/StateActivity.java
+++ app/src/main/java/ctec/stateapp/StateActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.TextView;
+import android.content.Intent;
+import android.view.View;
 
 public class StateActivity extends AppCompatActivity {
 
@@ -12,7 +14,30 @@ public class StateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_state);
+
     }
+
+
+    private void loadContent()
+    {
+       userText.setText(saveState.getUserName() + " is" + saveState.getAge()
+       + " years old and their tired status is" + saveState.getIsTired());
+
+    }
+
+    private void setupListeners()
+    {
+
+
+
+
+        Intent returnIntent = newIntent();
+        setResult(RESULT_OK, returnIntent);
+        finish();
+
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
